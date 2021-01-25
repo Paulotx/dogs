0535b251bdc5c8b8cbf6e32e6c10e22046cd6162
Paulo André <pauloandre0011@gmail.com>
2021-01-25 17:15:36 -0300

    implemented @unform at login

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,34 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    name: string;
     label: string;
 }
 
-const Input: React.FC<IInputProps> = ({ label, id, ...rest }) => {
+const Input: React.FC<IInputProps> = ({ name, label, id, ...rest }) => {
+    const inputRef = useRef(null);
+    const { fieldName, defaultValue, error, registerField } = useField(name);
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value',
+        });
+    }, [fieldName, registerField]);
+
     return (
         <Container>
             <label htmlFor={`${id}`}>{label}</label>
-            <input {...rest} id={id} />
+            <input
+                id={id}
+                ref={inputRef}
+                defaultValue={defaultValue}
+                {...rest}
+            />
             <p>Error</p>
         </Container>
     );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,44 +1,57 @@
-import React, { useCallback, useState, FormEvent } from 'react';
+import React, { useCallback, FormEvent } from 'react';
+import { Form } from '@unform/web';
+
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import { Container } from './styles';
 
+interface ICreteSession {
+    username: string;
+    password: string;
+}
+
 const Login: React.FC = () => {
-    const [username, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = useCallback(
-        (event: FormEvent) => {
-            event.preventDefault();
-
-            fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
+    const handleSubmit = useCallback((data: ICreteSession) => {
+        console.log(data);
+        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: data.username,
+                password: data.password,
+            }),
+        })
+            .then(response => {
+                console.log(response);
+                return response.json();
             })
-                .then(response => {
-                    console.log(response);
-                    return response.json();
-                })
-                .then(json => {
-                    console.log(json);
-                });
-        },
-        [username, password],
-    );
+            .then(json => {
+                console.log(json);
+            });
+    }, []);
 
     return (
         <Container>
             <h1>Login</h1>
 
-            <form onSubmit={event => handleSubmit(event)}>
-                <Input label="Usuário" type="text" id="username" />
-                <Input label="Senha" id="password" type="password" />
+            <Form onSubmit={handleSubmit}>
+                <Input
+                    name="username"
+                    label="Usuário"
+                    type="text"
+                    id="username"
+                />
+                <Input
+                    name="password"
+                    label="Senha"
+                    id="password"
+                    type="password"
+                />
                 <Button type="submit">Entrar</Button>
-            </form>
+            </Form>
         </Container>
     );
 };
